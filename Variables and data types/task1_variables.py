a68d357393b81bca436972bde3cfe34ed00f191b
Mueza Ejaz <159851369+Mueza-Ejaz@users.noreply.github.com>
2025-02-17 21:46:48 +0500

    Add files via upload

---
+++ Variables and data types/task1_variables.py
@@ -0,0 +1,31 @@
+# Task 1: Understanding Variables & Printing Output:
+
+# Type of string(str):
+
+str1 = "Hello World"
+print(str1) # Hello World
+print(type(str1)) # <class 'str'>
+
+
+# Type of Integer(int):
+
+int1 = -25
+print(int1) # -25
+print(type(int1)) # <class 'int'> 
+
+
+# Type of Float(decimal):
+
+dec1 = 25.5
+print(dec1) # 25.5
+print(type(dec1)) # <class 'float'>
+
+
+# Type of Boolean:
+
+num1 = 4
+num2 = 45
+print(num1<num2) # True
+print(num1>=num2) # False
+print(type(num1<num2)) # <class 'bool'>
+
