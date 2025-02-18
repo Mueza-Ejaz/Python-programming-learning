e224f5e12bbf5f8186775d2474b612ee8105269b
Mueza Ejaz <159851369+Mueza-Ejaz@users.noreply.github.com>
2025-02-18 14:47:58 +0500

    Chapter-1

---
+++ Variables and data types/task4_calculator.py
@@ -0,0 +1,19 @@
+ 
+ # Simple Calculator:
+
+firstNum = float(input("Enter the first number:"))
+secondNum = float(input("Enter the second number:"))
+
+
+addition = firstNum+secondNum 
+subtraction = firstNum-secondNum 
+multiplication = firstNum*secondNum 
+division = firstNum/secondNum
+
+ # Display the results:
+
+print("\nResults:")
+print(f"{firstNum} + {secondNum} = {addition}")  
+print(f"{firstNum} - {secondNum} = {subtraction}")  
+print(f"{firstNum} × {secondNum} = {multiplication}")  
+print(f"{firstNum} ÷ {secondNum} = {division}")  
---
+++ Variables and data types/task5_string_formatting.py
@@ -0,0 +1,10 @@
+# String Formatting & F-strings:
+
+# - Display output using formatted strings.
+
+name  = input("Enter your name:")
+age = input("Mention your age:")
+
+print(f"Hello {name}, you are {age} years old.") # f-strings format
+print("Hello {}, you are {} years old.".format(name,age)) # .format()
+
